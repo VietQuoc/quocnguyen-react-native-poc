53725d08dc918fa8e4dc0e8f8ead0125ccec6c66
Quoc Nguyen <quoc.nguyen@codecomplete.jp>
2021-12-07 15:03:30 +0700

    Splash Screen Setting Up

--- android/app/src/main/java/com/quocnguyen/poc/MainActivity.java
+++ android/app/src/main/java/com/quocnguyen/poc/MainActivity.java
@@ -1,9 +1,21 @@
 package com.quocnguyen.poc;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
 
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
